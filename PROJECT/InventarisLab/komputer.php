bfbe16bea8021b414dc2412d5a64f86251d1d6df
aisyahsa <aisyahsa.asa@gmail.com>
2016-04-14 00:48:21 +0700

    secenario computer
    do: coding scenario inventaris komputer
    todo: coding
    kendala: banyak tugas pak

---
+++ PROJECT/InventarisLab/komputer.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>&nbsp;</p>
+<form id="form1" name="form1" method="post" action="">
+  <table width="1077" height="527" border="1">
+    <tr>
+      <td width="115">&nbsp;</td>
+      <td width="37">&nbsp;</td>
+      <td width="134">&nbsp;</td>
+      <td width="91">&nbsp;</td>
+      <td width="104">&nbsp;</td>
+      <td width="263">&nbsp;</td>
+      <td width="287"><input type="submit" name="insert2" id="insert2" value="Search" /></td>
+    </tr>
+    <tr>
+      <td>Kategori</td>
+      <td>&nbsp;</td>
+      <td>Kategori</td>
+      <td>Komputer</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><input type="submit" name="komputer" id="komputer" value="Komputer" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td><input type="submit" name="non-komputer" id="non-komputer" value="Non-Komputer" /></td>
+      <td>&nbsp;</td>
+      <td><strong>ID-Barang</strong></td>
+      <td><input name="merk2" type="text" id="merk2" value="" /></td>
+      <td>&nbsp;</td>
+      <td><strong>Chipset Nortbridge</strong></td>
+      <td><input name="merk" type="text" id="merk" value="" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><strong>Lokasi</strong></td>
+      <td><input name="merk3" type="text" id="merk3" value="" /></td>
+      <td>&nbsp;</td>
+      <td><strong>Chipset Shoutbridge</strong></td>
+      <td><input name="merk9" type="text" id="merk9" value="" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><strong>Merek</strong></td>
+      <td><input name="merk4" type="text" id="merk4" value="" /></td>
+      <td>&nbsp;</td>
+      <td><strong>Tipe RAM</strong></td>
+      <td><input name="merk10" type="text" id="merk10" value="" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><strong>Kecepatan (Ghz)</strong></td>
+      <td><input name="merk5" type="text" id="merk5" value="" /></td>
+      <td>&nbsp;</td>
+      <td><strong>Kapasitas</strong></td>
+      <td><input name="merk11" type="text" id="merk11" value="" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><strong>Socket</strong></td>
+      <td><input name="merk6" type="text" id="merk6" value="" /></td>
+      <td>&nbsp;</td>
+      <td><strong>PC</strong></td>
+      <td><input name="merk12" type="text" id="merk12" value="" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><strong>Merk Motherboard</strong></td>
+      <td><input name="merk7" type="text" id="merk7" value="" /></td>
+      <td>&nbsp;</td>
+      <td><strong>MAC Adress</strong></td>
+      <td><input name="merk13" type="text" id="merk13" value="" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><strong>Model</strong></td>
+      <td><input name="merk8" type="text" id="merk8" value="" /></td>
+      <td>&nbsp;</td>
+      <td><strong>Tahun Pembelian</strong></td>
+      <td><label for="select"></label>
+        <select name="select" id="select">
+      </select></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input name="insert" type="submit" id="insert" value="Insert" /></td>
+      <td><input type="submit" name="update" id="update" value="Update" /></td>
+      <td>&nbsp;</td>
+    </tr>
+  </table>
+  <p>&nbsp;</p>
+</form>
+<p>&nbsp;</p>
+</body>
+</html>
