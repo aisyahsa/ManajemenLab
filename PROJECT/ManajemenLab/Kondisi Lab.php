4e3c1bb51a94530c0e70bc3c3cba646fc24da797
Alia Firdauzy <alia.firdauzy@yahoo.com>
2016-04-13 23:19:18 +0700

    .

---
+++ PROJECT/ManajemenLab/Kondisi Lab.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>&nbsp;</p>
+<form id="form1" name="form1" method="post" action="">
+  <table width="972" height="164" border="1">
+    <tr>
+      <td width="86">ID Komputer</td>
+      <td width="192"><select name="jenisBarang" id="jenisBarang">
+        <option value="Comp01">Comp01</option>
+      </select></td>
+      <td width="192">&nbsp;</td>
+      <td width="135">Keterangan</td>
+      <td width="333"><textarea name="deskripsi" id="deskripsi" cols="45" rows="5"></textarea></td>
+    </tr>
+    <tr>
+      <td>Kondisi</td>
+      <td><input name="Baik" type="radio" id="radio" value="Baik" />
+      <label><input name="Baik" type="text" id="Baik" value="Baik" />
+      </label></td>
+      <td><input type="radio" name="radio2" id="radio2" value="radio2" />
+      <input name="Buruk" type="text" id="Buruk" value="Buruk" /></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="save" id="save" value="Save" /></td>
+    </tr>
+  </table>
+  <p>KONDISI BARANG</p>
+  <p>&nbsp;</p>
+  <p>&nbsp; </p>
+</form>
+<p>&nbsp;</p>
+</body>
+</html>
